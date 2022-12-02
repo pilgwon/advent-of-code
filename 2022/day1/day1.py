a34682abc7279bda1e0a1fc9a17fb50e36547c89
pilgwon <rlavlfrnjs12@gmail.com>
2022-12-02 13:10:41 +0900

    :sparkles: day1 part two

--- 2022/day1/day1.py
+++ 2022/day1/day1.py
@@ -13,3 +13,25 @@ for index in range(len(input_data)):
     continue
   calories += int(input_data[index])
 print(max_calories)
+
+# day1 part two
+delta_list = []
+first = 0
+second = 0
+third = 0
+calories = 0
+for index in range(len(input_data)):
+  if input_data[index] == "":
+    if calories > first:
+      third = second
+      second = first
+      first = calories
+    elif calories > second:
+      third = second
+      second = calories
+    elif calories > third:
+      third = calories
+    calories = 0
+    continue
+  calories += int(input_data[index])
+print(first+second+third)
