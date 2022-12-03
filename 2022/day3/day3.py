46112fbabf3646746958a4c82892ae6e7f9ba3ce
pilgwon <rlavlfrnjs12@gmail.com>
2022-12-03 23:22:45 +0900

    :sparkles: day3 part two

--- 2022/day3/day3.py
+++ 2022/day3/day3.py
@@ -1,6 +1,12 @@
 input_file = open('./input.txt', 'r')
 input_data = input_file.read().rstrip().split('\n')
 
+def scoreOf(item):
+  if item > "a":
+    return ord(item) - 96
+  else:
+    return ord(item) - 64 + 26
+
 # day3 part one
 
 score = 0
@@ -12,8 +18,19 @@ for index in range(len(input_data)):
   right = rucksack[slice(len(rucksack)//2, len(rucksack))]
   intersection = list(set(left).intersection(right))
   for item in intersection:
-    if item > "a":
-      score += ord(item) - 96
-    else:
-      score += ord(item) - 64 + 26
+    score += score(item)
+print(score)
+
+# day3 part two
+
+score = 0
+for index in range(0, len(input_data), 3):
+  if input_data[index] == "":
+    break
+  first = input_data[index]
+  second = input_data[index+1]
+  third = input_data[index+2]
+  intersection = set(first).intersection(second).intersection(third)
+  for item in intersection:
+    score += scoreOf(item)
 print(score)
