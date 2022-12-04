a01be50bd2f77f08dca37965d5138ef61bf88fcb
pilgwon <rlavlfrnjs12@gmail.com>
2022-12-04 21:31:54 +0900

    :sparkles: day4 part two

--- 2022/day4/day4.py
+++ 2022/day4/day4.py
@@ -19,3 +19,22 @@ for index in range(len(input_data)):
   if check_fully_contains(left, right):
     pairs += 1
 print(pairs)
+
+# day4 part two
+
+def check_overlaps(lhs, rhs):
+  (lhs_start, lhs_end) = [int(x) for x in lhs.split('-')]
+  (rhs_start, rhs_end) = [int(x) for x in rhs.split('-')]
+  return (
+    (lhs_start <= rhs_end and lhs_end >= rhs_end) or
+    (rhs_start <= lhs_end and rhs_end >= lhs_end)
+  )
+
+pairs = 0
+for index in range(len(input_data)):
+  if input_data[index] == "":
+    break
+  (left, right) = input_data[index].split(',')
+  if check_overlaps(left, right):
+    pairs += 1
+print(pairs)
