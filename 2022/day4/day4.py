23fb509a05a08ca033b1481df74baadc4e337649
pilgwon <rlavlfrnjs12@gmail.com>
2022-12-04 21:31:45 +0900

    :sparkles: day4 part one

---
+++ 2022/day4/day4.py
@@ -0,0 +1,21 @@
+input_file = open('./input.txt', 'r')
+input_data = input_file.read().rstrip().split('\n')
+
+# day4 part one
+
+def check_fully_contains(lhs, rhs):
+  (lhs_start, lhs_end) = [int(x) for x in lhs.split('-')]
+  (rhs_start, rhs_end) = [int(x) for x in rhs.split('-')]
+  return (
+    (lhs_start <= rhs_start and rhs_end <= lhs_end) or
+    (rhs_start <= lhs_start and lhs_end <= rhs_end)
+  )
+
+pairs = 0
+for index in range(len(input_data)):
+  if input_data[index] == "":
+    break
+  (left, right) = input_data[index].split(',')
+  if check_fully_contains(left, right):
+    pairs += 1
+print(pairs)
